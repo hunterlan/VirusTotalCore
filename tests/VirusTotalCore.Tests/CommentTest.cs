31c96b528b8a853048f569979c03d1bfd47cd9b7
hunterlan <konstantin.sharykin@outlook.com>
2026-05-09 10:39:14 +0200

    refactor: move "Domains" resources to separate project (#35)

--- tests/VirusTotalCore.Tests/CommentTest.cs
+++ tests/VirusTotalCore.Tests/CommentTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using VirusTotalCore.Domains.Endpoints;
 using VirusTotalCore.Endpoints;
 using VirusTotalCore.Common.Models.Comments;
 using VirusTotalCore.Common.Models.Comments.Vote;
